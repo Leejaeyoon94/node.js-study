cb31012a0efef15822a352a1d46f7698bdded3cc
JaeYoon <ljayoon@gmail.com>
2021-06-24 14:41:49 +0900

    1

---
+++ notes-app/app.js
@@ -0,0 +1,12 @@
+// const add = require("./utils");
+
+// // const name = "jaeyoon";
+
+// const sum = add(4, -2);
+// console.log(sum);
+
+const getNotes = require("./notes");
+
+const msg = getNotes();
+
+console.log(msg);
