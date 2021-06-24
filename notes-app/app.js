395cdae1b441139803e750741e48d580e14b06a1
JaeYoon <ljayoon@gmail.com>
2021-06-24 15:34:34 +0900

    2

--- notes-app/app.js
+++ notes-app/app.js
@@ -4,9 +4,23 @@
 
 // const sum = add(4, -2);
 // console.log(sum);
+const chalk = require("chalk");
 
+// const validator = require("validator");
 const getNotes = require("./notes");
 
-const msg = getNotes();
+const command = process.argv[2];
 
-console.log(msg);
+if (command === "add") {
+  console.log("adding note!");
+} else if (command === "remove") {
+  console.log("remove note!");
+}
+// const msg = getNotes();
+
+// console.log(msg);
+// const greenMsg = chalk.green.bold("success");
+// console.log(greenMsg);
+// // console.log(validator.isURL("navercom"));
+
+// console.log(process.argv);
